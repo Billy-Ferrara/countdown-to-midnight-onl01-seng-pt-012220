b0adfce6771ad31ad0d83810b1f253602623d687
Billy Ferrara <willfira717@gmail.com>
2020-01-28 17:11:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,15 @@ def countdown
 while number = 0
 puts "Happy New Year!"
 end
+
+def countdown_with_sleep
+  number = 10
+  until number > 0
+  puts "#{number} SECOND(s)!"
+  number -= 1
+  
+  sleep 1.SECOND
+
+while number = 0
+puts "Happy New Year!"
+end
